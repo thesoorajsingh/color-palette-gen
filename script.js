62291351779bcfa809d3dbeec75ae474faa8411d
Sooraj M Singh <soorajsingh1999@gmail.com>
2021-12-23 14:03:07 +0530

    Initial commit of code files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+var changerButton = document.getElementById("changerButton");
+
+// on click listener
+
+
+changeColor = (id) => {
+    var color = document.getElementById(id);
+    var hex = '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
