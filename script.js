4e2258b480b9672dfd98cc7afd4c4adf357e37d8
Sooraj M Singh <soorajsingh1999@gmail.com>
2021-12-31 19:23:14 +0530

    HEX Value changes

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 
-
 var changerButton = document.getElementById("changerButton");
 
-// on click listener
-
-
-changeColor = (id) => {
+changeColor = (id,button) => {
     var color = document.getElementById(id);
+    var copy = document.getElementById(button);
     var hex = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    color.style.backgroundColor = hex;
+    console.log(typeof(hex))
+    copy.innerText = hex.toString().toUpperCase();
 }
+
+var generate = document.getElementById("changerButton").addEventListener("click", function() {
+    changeColor("section1","btn1");
+    changeColor("section2","btn2");
+    changeColor("section3","btn3");
+    changeColor("section4","btn4");
+    changeColor("section5","btn5");
+})
+// var copier = document.getElementById("color-title").addEventListener("click", function() {
+//     navigator.clipboard.writeText(copier.value)
+//     alert("Copied Hex Code Value!")
+// })
