372bb351f9ed5e5e90645d58802ae97ad0678328
Sooraj M Singh <soorajsingh1999@gmail.com>
2022-01-14 17:25:02 +0530

    changes

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
 var changerButton = document.getElementById("changerButton");
 
+document.onload = () => {
+changeColor("section1","btn1");
+changeColor("section2","btn2");
+changeColor("section3","btn3");
+changeColor("section4","btn4");
+changeColor("section5","btn5");
+}
+
 changeColor = (id,button) => {
     var color = document.getElementById(id);
     var copy = document.getElementById(button);
@@ -8,6 +16,7 @@ changeColor = (id,button) => {
     color.style.backgroundColor = hex;
     console.log(typeof(hex))
     copy.innerText = hex.toString().toUpperCase();
+    return hex.toString().toUpperCase();
 }
 
 var generate = document.getElementById("changerButton").addEventListener("click", function() {
